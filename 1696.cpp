dbec64360c94ddd43385a47d299f30c19b13f0b5
我还能孩子多久、... <kans2298@gmail.com>
2019-07-18 21:24:57 +0800

    由 Cloud Studio 更新

---
+++ 1696.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    while (cin>>s)
+        {
+        if (s[0]=='[')
+            {
+            
+            }
+        else
+            {
+            int a,b;
+            a=
+            }
+        }
+    return 0;
+}
